be502c4eed7deadc062864141baca3399de750dc
Lenovo <xuyangf01@gmail.com>
2019-08-24 18:29:03 +0800

    版本v0.12 后台修改为mysql

--- knowledge_sys/settings.py
+++ knowledge_sys/settings.py
@@ -79,12 +79,24 @@ WSGI_APPLICATION = 'knowledge_sys.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'knowledge_sys',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
