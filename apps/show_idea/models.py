206609018d1b7c7b21c0a263957da9dc82cd7972
Lenovo <xuyangf01@gmail.com>
2019-08-26 17:44:04 +0800

    版本v0.15,修改字段最大长度

--- apps/show_idea/models.py
+++ apps/show_idea/models.py
@@ -5,7 +5,7 @@ from ckeditor_uploader.fields import RichTextUploadingField
 
 # 大类主题
 class BigClassTheme(BaseModelApps):
-    bct_name = models.CharField("大类主题名", max_length=15, null=False)
+    bct_name = models.CharField("大类主题名", max_length=32, null=False)
     bct_describe = models.TextField("主题描述", max_length=500, default="此大类主题暂无描述")
 
     def __str__(self):
@@ -20,7 +20,7 @@ class BigClassTheme(BaseModelApps):
 class SubClassTheme(BaseModelApps):
     bct_id = models.ForeignKey(to="BigClassTheme", on_delete=models.CASCADE, to_field="t_id",
                                verbose_name="对应的大类主题ID")
-    sct_name = models.CharField("子类主题名", max_length=15, null=False)
+    sct_name = models.CharField("子类主题名", max_length=32, null=False)
     sct_describe = models.TextField("子主题描述", max_length=500, default="此子类主题暂无描述")
 
     def __str__(self):
@@ -37,7 +37,7 @@ class QuestionCalssTheme(BaseModelApps):
                                verbose_name="对应的大类主题ID")
     sct_id = models.ForeignKey(to="SubClassTheme", on_delete=models.CASCADE, to_field="t_id",
                                verbose_name="对应的子类主题ID")
-    qct_name = models.CharField("求助问题", max_length=15, null=False)
+    qct_name = models.CharField("求助问题", max_length=128, null=False)
     qct_method = RichTextUploadingField(null=True, blank=True, verbose_name="内容描述", default="暂无内容")
     qct_comment = RichTextUploadingField(verbose_name="内部评语指导", max_length=1024, null=True, blank=True, default="暂无内容")
 
